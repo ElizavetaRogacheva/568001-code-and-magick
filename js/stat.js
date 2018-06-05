90d6dd4e5828663b4e35eb897a598e83b429cbc7
Елизавета Рогачева <liza-97-97-97@mail.ru>
2018-06-06 01:20:59 +0500

    Рефакторинг, добавлены имена

--- js/stat.js
+++ js/stat.js
@@ -13,13 +13,25 @@ var renderCloud = function(ctx, cloudTopLeftX, cloudTopLeftY, cloudBottomRightX,
   ctx.fill();
 }
 
+var LEFT_TOP_X = 110;
+var LEFT_TOP_Y = 20;
+var RIGHT_BOTTOM_X = 630;
+var RIGHT_BOTTOM_Y = 290;
+var TEXT_START_X = 150;
+var TEXT_START_Y = 30;
+var COLUMN_X = 160;
+var COLUMN_Y = 250;
+var DISTANCE = 50;
+var COLUMN_WIDTH = 40;
+var COLUMN_MAX_HEIGHT = 150;
+
 window.renderStatistics = function(ctx, names, times) {
-  renderCloud(ctx, 110, 20, 630, 290, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, 100, 10, 620, 280, '#fff');
+  renderCloud(ctx, LEFT_TOP_X, LEFT_TOP_Y, RIGHT_BOTTOM_X, RIGHT_BOTTOM_Y, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, LEFT_TOP_X - 10, LEFT_TOP_Y - 10, RIGHT_BOTTOM_X - 10, RIGHT_BOTTOM_Y - 10, '#fff');
   ctx.font = '16px, PT Mono';
   ctx.fillStyle = "#000"
-  ctx.fillText('Ура вы победили!', 150, 30);
-  ctx.fillText('Список результатов:',150, 60);
+  ctx.fillText('Ура вы победили!', TEXT_START_X, TEXT_START_Y);
+  ctx.fillText('Список результатов:',TEXT_START_X, TEXT_START_Y + 30);
 
   ctx.fillStyle = '#000';
   var maxTime = 0;
@@ -31,11 +43,11 @@ window.renderStatistics = function(ctx, names, times) {
   }
 
   for (var i = 0; i < names.length; i++) {
-    var x = 160 + 90*i;
-    var y = 270 - 150 * times[i] / maxTime;
-    var width = 40;
-    var height = 150 * times[i] / maxTime;
-    ctx.fillRect(x, y, width, height);
+    var x = COLUMN_X + (COLUMN_WIDTH + DISTANCE)*i;
+    var y = COLUMN_Y - COLUMN_MAX_HEIGHT * times[i] / maxTime;
+    var height = COLUMN_MAX_HEIGHT * times[i] / maxTime;
+    ctx.fillRect(x, y, COLUMN_WIDTH, height);
+    ctx.fillText(names[i], x, COLUMN_Y + 20);
 
   }
 }
