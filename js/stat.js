fd617493674462599fa80a52a99f90f6b8487fa7
Елизавета Рогачева <liza-97-97-97@mail.ru>
2018-06-06 23:38:46 +0500

    Вынесение функции поиска максимума и построения гистограмм

--- js/stat.js
+++ js/stat.js
@@ -26,15 +26,7 @@ var DISTANCE = 50;
 var COLUMN_WIDTH = 40;
 var COLUMN_MAX_HEIGHT = 150;
 
-window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, LEFT_TOP_X, LEFT_TOP_Y, RIGHT_BOTTOM_X, RIGHT_BOTTOM_Y, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, LEFT_TOP_X - 10, LEFT_TOP_Y - 10, RIGHT_BOTTOM_X - 10, RIGHT_BOTTOM_Y - 10, '#fff');
-  ctx.font = '16px, PT Mono';
-  ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', TEXT_START_X, TEXT_START_Y);
-  ctx.fillText('Список результатов:', TEXT_START_X, TEXT_START_Y + 30);
-
-
+var chooseMax = function (times) {
   var maxTime = 0;
 
   for (var j = 0; j < times.length; j++) {
@@ -42,7 +34,10 @@ window.renderStatistics = function (ctx, names, times) {
       maxTime = times[j];
     }
   }
+  return maxTime;
+};
 
+var renderHistogram = function (ctx, times, names, maxTime) {
   for (var i = 0; i < names.length; i++) {
     var x = COLUMN_X + (COLUMN_WIDTH + DISTANCE) * i;
     var y = COLUMN_Y - COLUMN_MAX_HEIGHT * times[i] / maxTime;
@@ -53,4 +48,17 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.fillText(names[i], x, COLUMN_Y + 20);
 
   }
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, LEFT_TOP_X, LEFT_TOP_Y, RIGHT_BOTTOM_X, RIGHT_BOTTOM_Y, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, LEFT_TOP_X - 10, LEFT_TOP_Y - 10, RIGHT_BOTTOM_X - 10, RIGHT_BOTTOM_Y - 10, '#fff');
+  ctx.font = '16px, PT Mono';
+  ctx.fillStyle = '#000';
+  ctx.fillText('Ура вы победили!', TEXT_START_X, TEXT_START_Y);
+  ctx.fillText('Список результатов:', TEXT_START_X, TEXT_START_Y + 30);
+
+  var maxTime = chooseMax(times);
+  renderHistogram(ctx, times, names, maxTime);
+
 };
