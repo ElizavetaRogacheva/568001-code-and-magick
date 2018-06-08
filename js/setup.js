a73020786386e2d641feaac95cd9c5354b8af193
Елизавета Рогачева <liza-97-97-97@mail.ru>
2018-06-08 19:45:54 +0500

    Создание массивов свойств персонажей

--- js/setup.js
+++ js/setup.js
@@ -3,4 +3,44 @@
 var userSetup = document.querySelector('.setup');
 userSetup.classList.remove('hidden');
 
+var wizardNames = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+
+var wizardSurnames = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+
+var coatColors = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+
+];
+
+eyesColors = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+
 
